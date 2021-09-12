c0c59b53ca4831a72bc54ca69cd6722492977c3f
gdlyeabkov <gdlyeabkov@gmail.com>
2021-09-12 22:59:39 +0300

    fixaddcontacts

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const express = require('express')
 const path = require('path')
 const app = express()
-// const server = require('http').Server(app)
-// const io = require("socket.io")(server)
+const server = require('http').Server(app)
+const io = require("socket.io")(server)
 const serveStatic = require('serve-static')
 
 app.use('/', serveStatic(path.join(__dirname, '/dist')))
@@ -42,12 +42,14 @@ mongoose.connect(url, connectionParams)
         console.error(`Error connecting to the database. \n${err}`);
     })
 
-
-
-// io.on('connection', (socket) => {
-//     socket.on('disconnect', function() {
-//     })
-// })
+app.get('/sockets/connect', (req, res) => {
+    // io.on('connection', (socket) => {
+    //     console.log(`Присоединился socket.id: ${socket.id}`)
+    //     socket.on('disconnect', function() {
+    //         console.log(`Отключился socket.id: ${socket.id}`)
+    //     })
+    // })
+})
 
 app.get('/contacts/create', (req, res) => {
 
@@ -109,28 +111,62 @@ app.get('/contacts/add', (req, res) => {
     res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, X-Access-Token, X-Socket-ID, Content-Type");
     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
 
-    ContactModel.updateOne({ _id: req.query.contactid },
-        { $push: 
-            { 
-                contacts: [
-                    {
-                        id: req.query.othercontactid,
-                        name: req.query.contactname,
-                        phone: req.query.contactphone,
-                        avatar: "empty"
-                    }
-                ]
-                
-            }
-    }, (err, user) => {
+    let insertContact = true
+    let query = ContactModel.findOne({ _id: req.query.contactid })
+    query.exec((err, currentContact) => {
         if(err){
-            return res.json({ "status": "Error" })    
+            return res.json({ status: "Error" })
         }
-        return res.json({ "status": "OK" })
+        currentContact.contacts.map(bindingContact => {
+            console.log(`new Map(bindingContact).get('id'): ${new Map(bindingContact).get('id')}, req.query.othercontactid: ${req.query.othercontactid}`)
+            if(new Map(bindingContact).get('id').includes(req.query.othercontactid)){
+                insertContact = false
+            }
+        })
+        if(insertContact){
+            let queryOfOtherContact = ContactModel.findOne({ _id: req.query.othercontactid })
+            queryOfOtherContact.exec((err, otherContact) => {
+                if(err) {
+                    return res.json({ "status": "Error" })
+                }
+                ContactModel.updateOne({ _id: req.query.contactid },
+                    { $push: 
+                        { 
+                            contacts: [
+                                {
+                                    id: req.query.othercontactid,
+                                    name: otherContact.name,
+                                    phone: otherContact.phone,
+                                    avatar: "empty"
+                                }
+                            ]
+                        }
+                }, (err, user) => {
+                    ContactModel.updateOne({ _id: req.query.othercontactid },
+                        { $push: 
+                            { 
+                                contacts: [
+                                    {
+                                        id: req.query.contactid,
+                                        name: currentContact.name,
+                                        phone: currentContact.phone,
+                                        avatar: "empty"
+                                    }
+                                ]
+                            }
+                    }, (err, user) => {
+                        if(err){
+                            return res.json({ "status": "Error" })    
+                        }
+                        return res.json({ "status": "OK" })
+                    })
+                })
+            })
+        }
     })
 })
 
-app.get('/contacts/add', (req, res) => {
+app.get('/contacts/messages/add', (req, res) => {
 
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Credentials', true);
